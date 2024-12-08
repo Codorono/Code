0dd51fe49ebd6c3b5a877511437d12bdfb685f17
Codorono <codorono@live.com>
2024-12-08 13:41:26 -0800

    Updated.

---
+++ C++/GetCurrentDirectory.cpp
@@ -0,0 +1,68 @@
+﻿//###########################################################################
+//
+// Implementation of get current directory functionality.
+//
+// Copyright (c) Paul MacDonald. All rights reserved.
+//
+//###########################################################################
+
+#include "StdAfx.h"
+
+#include <WinApi/FilePath.h>
+
+#include <FileSys/FilePath.h>
+
+//======================================================================================================================
+
+std::wstring Ps::GetCurrentDirectoryW()
+{
+	std::wstring strResult;
+
+	//allocate stack buffer
+
+	std::array<wchar_t, _PMAC_STACK_ALLOC_CHARS> arrStackBuffer;
+
+	//get current directory
+
+	size_t zResultSize{ Pw::GetCurrentDirectoryW(arrStackBuffer.size(), arrStackBuffer.data()) };
+
+	//success
+
+	if (zResultSize < arrStackBuffer.size())
+	{
+		strResult.assign(arrStackBuffer.data(), zResultSize);
+	}
+
+	//buffer too small
+
+	else
+	{
+		//loop until it fits
+
+		for (;;)
+		{
+			//allocate heap buffer
+
+			size_t zBufferSize{ zResultSize };
+
+			std::unique_ptr<wchar_t[]> ptrHeapBuffer{ std::make_unique_for_overwrite<wchar_t[]>(zBufferSize) };
+
+			//get current directory
+
+			zResultSize = Pw::GetCurrentDirectoryW(zBufferSize, ptrHeapBuffer.get());
+
+			//success
+
+			if (zResultSize < zBufferSize)
+			{
+				strResult.assign(ptrHeapBuffer.get(), zResultSize);
+
+				break;
+			}
+		}
+	}
+
+	return strResult;
+}
+
+//======================================================================================================================
---
+++ C++/GetWindowText.cpp
@@ -0,0 +1,47 @@
+﻿//###########################################################################
+//
+// Implementation of get window text functionality.
+//
+// Copyright (c) Paul MacDonald. All rights reserved.
+//
+//###########################################################################
+
+#include "StdAfx.h"
+
+#include <WinApi/Window.h>
+
+#include <Text/Window.h>
+
+//======================================================================================================================
+
+std::wstring Ps::GetWindowTextW(HWND hWnd, size_t zMaxSize)
+{
+	std::wstring strResult;
+
+	//get buffer size
+
+	if (zMaxSize == SIZE_MAX)
+	{
+		zMaxSize = Pw::GetWindowTextLengthW(hWnd);
+	}
+
+	//ensure not empty
+
+	if (zMaxSize != 0)
+	{
+		//get window text
+
+		strResult.resize_and_overwrite
+		(
+			zMaxSize,
+			[&](wchar_t* pchString, size_t zStringSize)
+			{
+				return Pw::GetWindowTextW(hWnd, pchString, zStringSize + 1);
+			}
+		);
+	}
+
+	return strResult;
+}
+
+//======================================================================================================================
